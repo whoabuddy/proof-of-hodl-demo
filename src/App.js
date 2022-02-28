f1b36d081ffdfd85bc95c8d654e727504d3b9ba9
Jason Schrader <whoabuddy@users.noreply.github.com>
2022-02-28 11:14:06 -0700

    feat: add basic qr code scanner poc

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { QrReader } from "react-qr-reader";
+import { useState } from "react";
 
 function App() {
+  const [data, setData] = useState("No result");
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>Proof of Hodl Demo App</p>
+      <QrReader
+        constraints={{ facingMode: "environment" }}
+        onResult={(result, error) => {
+          if (!!result) {
+            setData("loading...");
+            checkProofOfHodl(result?.text).then((answer) => setData(answer));
+          }
+          if (!!error) {
+            console.info(error);
+          }
+        }}
+        style={{ width: "100%" }}
+      />
+      <p>{data}</p>
     </div>
   );
 }
 
 export default App;
+
+async function checkProofOfHodl(address) {
+  const response = await fetch(
+    `https://citycoins-api.citycoins.workers.dev/tools/proof-of-hodl/mia/${address}`
+  );
+  const data = await response.json();
+  console.log(`data: ${JSON.stringify(data)}`);
+  return data.value.toString();
+}
